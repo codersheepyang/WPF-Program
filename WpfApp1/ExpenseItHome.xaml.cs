64bc2aa62ee3109c87c44039057a2a21b287f963
YangK <935046315@qq.com>
2020-04-27 19:03:58 +0800

    update

--- WpfApp1/ExpenseItHome.xaml.cs
+++ WpfApp1/ExpenseItHome.xaml.cs
@@ -24,5 +24,11 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            this.NavigationService.Navigate(expenseReportPage);
+        }
     }
 }
